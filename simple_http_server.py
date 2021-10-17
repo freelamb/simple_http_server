fb05cbfc171072f6564bbc2d11643f0d90f0b7ab
YanBo <yanbo@dian-yanbo.local>
2021-10-17 15:22:46 +0800

    delete signal SIGHUP for windows

--- simple_http_server.py
+++ simple_http_server.py
@@ -281,7 +281,6 @@ def main():
     server_address = ('', port)
 
     signal.signal(signal.SIGINT, signal_handler)
-    signal.signal(signal.SIGHUP, signal_handler)
     signal.signal(signal.SIGTERM, signal_handler)
 
     httpd = HTTPServer(server_address, SimpleHTTPRequestHandler)
